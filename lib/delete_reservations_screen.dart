e16db2d3fa1f4f22672bb18b9a4bc7f3b2905020
Eudald Sans <eudald.sans@gmail.com>
2025-05-12 16:34:44 +0200

    finish setting up reservation process

--- lib/delete_reservations_screen.dart
+++ lib/delete_reservations_screen.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
 
 // Firebase
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 // Widgets
-import 'package:reservations_app/widgets/date_selector.dart';
-import 'package:reservations_app/widgets/table_card.dart';
 import 'package:reservations_app/widgets/reserve_table_buttons.dart';
+
+import 'package:toastification/toastification.dart';
 
 // app
 import 'package:reservations_app/widgets/reservation_card.dart';
-import 'package:time_range_picker/time_range_picker.dart';
 
 class DeleteReservationsScreen extends StatefulWidget {
   const DeleteReservationsScreen({super.key});
@@ -47,23 +47,47 @@ class _DeleteReservationsScreenState extends State<DeleteReservationsScreen> {
                 child: ListView.builder(
                   itemCount: snapshot.data!.docs.length,
                   itemBuilder: (context, index) {
-                    return ValueListenableBuilder<DateTime>(
-                      valueListenable: selectedDateNotifier,
-                      builder: (context, DateTime value, child) {
-                        String tableName = 'Test';
+                    String tableName = 'Test';
 
-                        Timestamp startTime = snapshot.data!.docs[index].data()['startDate'];
-                        Timestamp endtTime = snapshot.data!.docs[index].data()['endDate'];
-                        DateTime selectedDate = DateTime.fromMillisecondsSinceEpoch(startTime.millisecondsSinceEpoch);
+                    Timestamp startTime = snapshot.data!.docs[index].data()['startDate'];
+                    Timestamp endtTime = snapshot.data!.docs[index].data()['endDate'];
+                    DateTime selectedDate = DateTime.fromMillisecondsSinceEpoch(startTime.millisecondsSinceEpoch);
 
-                        return ReservationCard(
-                          tableID: snapshot.data!.docs[index].data()['tableID'],
-                          selectedDate: selectedDate,
-                          reservationStart: startTime,
-                          reservationEnd: endtTime,
-                          tableName: tableName,
-                        );
-                      }
+                    return Row(
+                      children: [
+                        Expanded(
+                          child: ReservationCard(
+                            tableID: snapshot.data!.docs[index].data()['tableID'],
+                            selectedDate: selectedDate,
+                            reservationStart: startTime,
+                            reservationEnd: endtTime,
+                            tableName: tableName,
+                          ),
+                        ),
+                        CustomIconButton(
+                          icon: IconData(0xeeaa, fontFamily: 'MaterialIcons'),
+                          onPressed: () {
+                            try {
+                              FirebaseFirestore.instance
+                              .collection("reservations")
+                              .doc(snapshot.data!.docs[index].id)
+                              .delete()
+                              .then((_) {
+                                log("Reservation deleted");
+                                toastification.show(title: Text("Reservation deleted"),
+                                  autoCloseDuration: const Duration(seconds: 5),
+                                );
+                              });
+                            } on FirebaseException catch (e) {
+                              log(e.message!);
+                              toastification.show(title: Text(e.message!),
+                                autoCloseDuration: const Duration(seconds: 5),
+                                type: ToastificationType.error
+                              );
+                            }
+                          },
+                        ),
+                      ],
                     );
                   },
                 ),
