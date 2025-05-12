3f6c40c0aef58cf6ea4a22053d5235c6016d2824
Eudald Sans <eudald.sans@gmail.com>
2025-05-12 12:56:29 +0200

    added logic to ensure reservations can not overlap

--- lib/database/add_new_reservation.dart
+++ lib/database/add_new_reservation.dart
@@ -14,6 +14,13 @@ import 'package:time_range_picker/time_range_picker.dart';
 
 import 'package:reservations_app/widgets/time_ranges.dart';
 
+class CustomTimeRange {
+  final Timestamp startTime;
+  final Timestamp endTime;
+
+  const CustomTimeRange({required this.startTime, required this.endTime});
+}
+
 class ReserveTable extends StatefulWidget {
   final String tableId;
   final DateTime selectedDate;
@@ -28,32 +35,98 @@ class ReserveTable extends StatefulWidget {
   State<ReserveTable> createState() => _ReserveTableState();
 }
 
+bool isTimeSlotAvailable(CustomTimeRange currentSlot, CustomTimeRange occupiedSlot) {
+  log("currentSlot start: ${currentSlot.startTime.millisecondsSinceEpoch} - occupiedSlot start: ${occupiedSlot.startTime.millisecondsSinceEpoch}");
+  log("currentSlot end: ${currentSlot.endTime.millisecondsSinceEpoch} - occupiedSlot end: ${occupiedSlot.endTime.millisecondsSinceEpoch}");
+
+  // Check if current slot starts within occupied slot
+  if ((currentSlot.startTime.millisecondsSinceEpoch > occupiedSlot.startTime.millisecondsSinceEpoch) && (currentSlot.startTime.millisecondsSinceEpoch < occupiedSlot.endTime.millisecondsSinceEpoch)) {
+    log("Selected slot starts within occupied slot!");
+    toastification.show(
+      title: Text("Selected slot starts within occupied slot!"),
+      autoCloseDuration: const Duration(seconds: 10),
+      type: ToastificationType.error,
+    );
+
+    return false;
+  }
+
+  // Check if current slot ends within occupied slot
+  if ((currentSlot.endTime.millisecondsSinceEpoch > occupiedSlot.startTime.millisecondsSinceEpoch) && (currentSlot.endTime.millisecondsSinceEpoch < occupiedSlot.endTime.millisecondsSinceEpoch)) {
+    log("Selected slot ends within occupied slot!");
+    toastification.show(
+      title: Text("Selected slot ends within occupied slot!"),
+      autoCloseDuration: const Duration(seconds: 10),
+      type: ToastificationType.error,
+    );
+
+    return false;
+  }
+
+  // Check if occupied slot is within current slot
+  if ((currentSlot.startTime.millisecondsSinceEpoch <= occupiedSlot.startTime.millisecondsSinceEpoch) && (currentSlot.endTime.millisecondsSinceEpoch >= occupiedSlot.endTime.millisecondsSinceEpoch)) {
+    log("Selected slot is already reserved!");
+    toastification.show(
+      title: Text("Selected slot is already reserved!"),
+      autoCloseDuration: const Duration(seconds: 10),
+      type: ToastificationType.error,
+    );
+
+    return false;
+  }
+  
+
+  return true;
+}
+
 class _ReserveTableState extends State<ReserveTable> {
   var reservationStart = TimeOfDay(hour: 2, minute: 0);
   var reservationEnd = TimeOfDay(hour: 3, minute: 0);
   final MainTimeRange mainPicker = MainTimeRange();
 
-  Future<void> uploadReservationToDb() async {
+  List<CustomTimeRange> reservedSlots = [];
+
+  Future<bool> uploadReservationToDb() async {
+    final id = const Uuid().v4();
+
+    if (reservationEnd.isAtSameTimeAs(reservationStart)) {
+      log("Start and end times whould not be equal!");
+      toastification.show(
+        title: Text("Start and end times whould not be equal!"),
+        autoCloseDuration: const Duration(seconds: 10),
+        type: ToastificationType.error,
+      );
+
+      return false;
+    }
+
+    DateTime startDate = widget.selectedDate.add(Duration(
+      hours: mainPicker.reservationStart.hour, 
+      minutes: mainPicker.reservationStart.minute
+    ));
+    DateTime endDate = widget.selectedDate.add(Duration(
+      hours: mainPicker.reservationEnd.hour, 
+      minutes: mainPicker.reservationEnd.minute
+    ));
+
+    CustomTimeRange currentSlot = CustomTimeRange(startTime: Timestamp.fromDate(startDate), endTime: Timestamp.fromDate(endDate));
+
+    if (mainPicker.reservationStart.hour > mainPicker.reservationEnd.hour) {
+      endDate = endDate.add(Duration(days: 1));
+      log('Adding one day to end duration');
+    }
+
+    for (var occupiedSlot in reservedSlots) {
+      if (!isTimeSlotAvailable(currentSlot, occupiedSlot)) return false;
+    }
+
+    int startSecondsSinceEpoch = (startDate.millisecondsSinceEpoch/1000).toInt();
+    int endSecondsSinceEpoch = (endDate.millisecondsSinceEpoch/1000).toInt();
+
+    Timestamp start = Timestamp(startSecondsSinceEpoch, 0);
+    Timestamp end = Timestamp(endSecondsSinceEpoch, 0);
+
     try {
-      final id = const Uuid().v4();
-
-      DateTime startDate = widget.selectedDate.add(Duration(
-        hours: mainPicker.reservationStart.hour, 
-        minutes: mainPicker.reservationStart.minute
-      ));
-      DateTime endDate = widget.selectedDate.add(Duration(
-        hours: mainPicker.reservationEnd.hour, 
-        minutes: mainPicker.reservationEnd.minute
-      ));
-
-      int startSecondsSinceEpoch = (startDate.millisecondsSinceEpoch/1000).toInt();
-      int endSecondsSinceEpoch = (endDate.millisecondsSinceEpoch/1000).toInt();
-
-      Timestamp start = Timestamp(startSecondsSinceEpoch, 0);
-      Timestamp end = Timestamp(endSecondsSinceEpoch, 0);
-
-      print('Reserving from ${start} to ${end}');
-
       await FirebaseFirestore.instance.collection("reservations").doc(id).set({
         "userID": FirebaseAuth.instance.currentUser!.uid,
         "creationDate": FieldValue.serverTimestamp(),
@@ -69,7 +142,11 @@ class _ReserveTableState extends State<ReserveTable> {
       toastification.show(title: Text(e.message!),
         autoCloseDuration: const Duration(seconds: 5),
       );
+
+      return false;
     }
+
+    return true;
   }
 
   @override
@@ -99,6 +176,7 @@ class _ReserveTableState extends State<ReserveTable> {
                   }
 
                   List<Widget> pickerList = [];
+                  reservedSlots.clear();
 
                   if (snapshot.hasData && !snapshot.data!.docs.isEmpty) {
                     final filtered = snapshot.data!.docs
@@ -106,16 +184,20 @@ class _ReserveTableState extends State<ReserveTable> {
                       .toList();
                     
                     filtered.forEach((data) {
+                      DateTime start = DateTime.fromMillisecondsSinceEpoch(data['startDate'].millisecondsSinceEpoch);
+                      DateTime end  = DateTime.fromMillisecondsSinceEpoch(data['endDate'].millisecondsSinceEpoch);
+
+                      reservedSlots.add(CustomTimeRange(
+                        startTime: data['startDate'], 
+                        endTime: data['endDate'],
+                      ));
+
                       pickerList.add(
                         DisabledTimeRange(
                           disabledTime: TimeRange(
-                              startTime: TimeOfDay.fromDateTime(
-                                DateTime.fromMillisecondsSinceEpoch(data['startDate'].millisecondsSinceEpoch)
-                              ),
-                              endTime: TimeOfDay.fromDateTime(
-                                DateTime.fromMillisecondsSinceEpoch(data['endDate'].millisecondsSinceEpoch)
-                              ),
-                            )
+                            startTime: TimeOfDay.fromDateTime(start),
+                            endTime: TimeOfDay.fromDateTime(end),
+                          )
                         )
                       );
                     });
